df36d853afe853f1b098619df998081ac2148db1
Jose Garza <sventripikal@gmail.com>
2025-01-23 15:46:02 -0600

    ADDED FILES FROM PROJECT
    -constants
    -functions
    -main
    -CoffeeMachine
    -ExitException

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
+package machine
 
+import machine.model.ExitException
+
+fun main() {
+    while (true) {
+        try {
+            // map of machine supply status
+            val machineSupplyStatus = mutableMapOf(
+                MACHINE_WATER to MACHINE_WATER_DEFAULT,
+                MACHINE_MILK to MACHINE_MILK_DEFAULT,
+                MACHINE_BEANS to MACHINE_BEANS_DEFAULT,
+                MACHINE_CUPS to MACHINE_CUPS_DEFAULT,
+                MACHINE_MONEY to MACHINE_MONEY_DEFAULT
+            )
+            // prompt action
+            promptAction(machineSupplyStatus)
+            // catch exceptions
+        } catch (e: Exception) {
+            if (e is ExitException) break                       // exit loop
+            else println("\nInvalid entry - ${e.message}\n")    // print error
+        }
+    }
 }
---
+++ src/main/kotlin/model/ExitException.kt
@@ -0,0 +1,6 @@
+package machine.model
+
+/**
+ *  @throws Exception used to exit from while loop
+ */
+class ExitException: Exception()
